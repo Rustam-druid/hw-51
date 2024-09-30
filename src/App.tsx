bab390f5b1e5ebcd2f35cc66c69cf1509ab941dc
Mabo Rustam <kimlael01@mail.ru>
2024-09-30 13:32:40 +0600

    Tz-51 Start

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+const App = () => {
+
+
+
+    return (
+        <div className="App">
+            <div className='header'></div>
+            <div className='main'>
+                <button></button>
+            </div>
+            <div className='footer'></div>
+        </div>
+    );
+};
+
+export default App;
